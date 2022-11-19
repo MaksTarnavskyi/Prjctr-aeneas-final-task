c301e7ec296086f6de396dda6570b634b65c0776
MaksTarnavskyi <maksym.tarnavskyi@gmail.com>
2022-11-19 18:12:44 +0100

    feat: add fast_api serving

---
+++ serving/fast_api.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from aeneas.predictor import AeneasPredictor
+
+
+class Payload(BaseModel):
+    texts: List[str]
+
+
+class Prediction(BaseModel):
+    output: List[str]
+
+
+app = FastAPI()
+predictor = AeneasPredictor(transformer_name = 'distilroberta-base', model_dir = 'models/distilroberta-base')
+
+
+@app.get("/health_check")
+def health_check() -> str:
+    return "ok"
+
+
+@app.post("/predict", response_model=Prediction)
+def predict(payload: Payload) -> Prediction:
+    prediction = predictor.predict(text=payload.text)
+    return Prediction(probs=prediction.tolist())
