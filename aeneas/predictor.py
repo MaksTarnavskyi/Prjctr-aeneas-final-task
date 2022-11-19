84099f84fad5f493ef421457e4af54cb9f7761a6
MaksTarnavskyi <maksym.tarnavskyi@gmail.com>
2022-11-19 17:59:59 +0100

    feat: add predictor

--- aeneas/predictor.py
+++ aeneas/predictor.py
@@ -5,6 +5,7 @@ from transformers import AutoModelForTokenClassification, DataCollatorForTokenCl
 from aeneas.tokenizer import AeneasTokenizer
 from aeneas.data_loader import AeneasDataLoader
 import numpy as np
+from typing import List
 from utils.helpers import init_logger
 
 logger = init_logger(__name__)
@@ -56,3 +57,24 @@ class AeneasPredictor:
         logger.info("Missing labels added")
         self.aeneas_data_loader.save_predicted_labels_to_file(extended_predicted_word_labels, output_file)
         logger.info(f"Labels saved in {output_file}")
+
+    def predict_for_online(self, input_texts: List[str]):
+
+        tokenized_dataset = self.aeneas_data_loader.load_test_sentences(input_texts)
+        logger.info("Tokenized dataset loaded")
+        token_predictions, token_labels_to_ignore, _ = self.aeneas_model.predict(tokenized_dataset)
+        token_predicted_labels = np.argmax(token_predictions, axis=2)  # predictions[:, :, 1]
+
+        logger.info("Labels predicted")
+        # Remove ignored index (special tokens)
+        word_predicted_labels = [
+            [p for (p, l) in zip(prediction, label) if l != -100]
+            for prediction, label in zip(token_predicted_labels, token_labels_to_ignore)
+        ]
+
+        extended_predicted_word_labels = self.aeneas_data_loader.add_missing_labels_for_long_sentences(
+                                                                word_predicted_labels, tokenized_dataset)
+        logger.info("Missing labels added")
+        output = self.aeneas_data_loader.return_predicted_labels(extended_predicted_word_labels)
+        return output
+        
